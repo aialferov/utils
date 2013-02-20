d5d83b70f3860c4abc0a45e08d19d0da0e788bd5
casper <casper.zx@gmail.com>
2013-02-21 02:53:00 +0400

    Added get_key function.
    Returns value by a specified key of the running application.

--- src/utils_app.erl
+++ src/utils_app.erl
@@ -1,5 +1,11 @@
 -module(utils_app).
--export([get_env/1]).
+-export([get_env/1, get_key/1]).
 
 get_env(Pars) -> lists:flatten([fun() -> case application:get_env(Par) of
 	{ok, Val} -> {Par, Val}; undefined -> [] end end() || Par <- Pars]).
+
+get_key(Key) -> get_key(application:get_application(), {key, Key}).
+get_key({ok, Application}, {key, Key}) ->
+	get_key(application:get_key(Application, Key), ok);
+get_key({ok, Value}, ok) -> Value;
+get_key(undefined, _) -> [].
