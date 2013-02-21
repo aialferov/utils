0958d44c15e98da06d2a63e61d0c4c7be8113fa8
casper <casper.zx@gmail.com>
2013-02-22 02:57:46 +0400

    Added query_string and header_string functions.

--- src/utils_http.erl
+++ src/utils_http.erl
@@ -1,5 +1,7 @@
 -module(utils_http).
+
 -export([read_query/1]).
+-export([header_string/1, query_string/1]).
 
 read_query(Query) -> read_query(Query, [], []).
 
@@ -13,3 +15,12 @@ read_query([], Item, Query) ->
 
 complete_query_item(Item, [Field|Query]) ->
 	[{lists:reverse(Field), http_uri:decode(lists:reverse(Item))}|Query].
+
+query_string(Params) -> kv_string(Params, fun(K, V, Acc) ->
+	[Acc, case Acc of [] -> []; Acc -> "&" end, K, "=", V] end).
+
+header_string(Params) -> kv_string(Params, fun(K, V, Acc) ->
+	[Acc, case Acc of [] -> []; Acc -> ", " end, K, "=", "\"", V, "\""] end).
+
+kv_string(Params, Builder) -> lists:foldl(fun({K, V}, Acc) ->
+	lists:append(Builder(K, V, Acc)) end, [], Params).
