cc6e4b877d3370dd2649ea2203392eb5da8c595a
casper <casper.zx@gmail.com>
2013-05-20 13:52:36 +0400

    The http url function now supports encode.
    The encode* options is now supported by the utils_http:url function.
    The encode_k and encode_v are renamed to encode_key and encode_value
    accordingly.

--- src/utils_http.erl
+++ src/utils_http.erl
@@ -7,7 +7,7 @@
 
 -module(utils_http).
 
--export([url/2, url/3]).
+-export([url/2, url/3, url/4]).
 -export([read_query/1]).
 -export([header_string/1, header_string/2, query_string/1, query_string/2]).
 
@@ -24,7 +24,7 @@ read_query([], Item, Query) ->
 complete_query_item(Item, [Field|Query]) ->
 	[{lists:reverse(Field), http_uri:decode(lists:reverse(Item))}|Query].
 
-query_string(Params) -> query_string(Params, []).
+query_string(Params) -> query_string(Params, no_encode).
 query_string(Params, Encode) -> kv_string(Params, Encode, fun({K, V}, Acc) ->
 	[Acc, case Acc of [] -> []; Acc -> "&" end, K, "=", V] end).
 
@@ -35,12 +35,16 @@ header_string(Params, Encode) -> kv_string(Params, Encode, fun({K, V}, Acc) ->
 kv_string(Params, Encode, Builder) -> lists:foldl(fun(Pair, Acc) ->
 	lists:append(Builder(encode(Pair, Encode), Acc)) end, [], Params).
 
-encode({K, V}, encode_k) -> {http_uri:encode(K), V};
-encode({K, V}, encode_v) -> {K, http_uri:encode(V)};
+encode({K, V}, encode_key) -> {http_uri:encode(K), V};
+encode({K, V}, encode_value) -> {K, http_uri:encode(V)};
 encode({K, V}, encode) -> {http_uri:encode(K), http_uri:encode(V)};
-encode(Pair, []) -> Pair.
+encode(Pair, no_encode) -> Pair.
 
-url(Url, []) -> Url;
-url(Url, Params) -> Url ++ "?" ++ utils_http:query_string(Params).
-url(Url, Params, []) -> url(Url, Params);
-url(Url, Params, ParamString) -> url(Url, Params) ++ "&" ++ ParamString.
+url(Url, Params) -> url(Url, Params, no_encode).
+url(Url, [], _Encode) -> Url;
+url(Url, Params, Encode) when is_atom(Encode) ->
+	Url ++ "?" ++ utils_http:query_string(Params, Encode);
+url(Url, Params, ParamString) -> url(Url, Params, ParamString, no_encode).
+url(Url, Params, [], Encode) -> url(Url, Params, Encode);
+url(Url, Params, ParamString, Encode) ->
+	url(Url, Params, Encode) ++ "&" ++ ParamString.
