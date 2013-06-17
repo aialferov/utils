2c4cbd7f7aa312a31833d2e0063008e539c3f417
casper <casper.zx@gmail.com>
2013-06-18 02:50:43 +0400

    Added another version of keyfind.
    The existing keyfind function is renamed to keyfind2.
    Added the new keyfind function which returns more strict values
    in both cases success or error.

--- src/utils_lists.erl
+++ src/utils_lists.erl
@@ -6,7 +6,13 @@
 %%%-------------------------------------------------------------------
 
 -module(utils_lists).
--export([keyfind/2]).
+-export([keyfind/2, keyfind2/2]).
 
-keyfind(Key, List) -> case lists:keyfind(Key, 1, List) of
-	{Key, Value} -> Value; false -> false end.
+keyfind(Key, List) -> keyfind(v1, Key, List).
+keyfind2(Key, List) -> keyfind(v2, Key, List).
+
+keyfind(V, Key, List) -> case lists:keyfind(Key, 1, List) of
+	{Key, Value} -> case V of v1 -> {ok, Value}; v2 -> Value end;
+	false -> case V of v1 -> {error, not_found}; v2 -> false end
+end.
+
