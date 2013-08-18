fc9192f8517e902062fceb227515d55e52d7e7a3
casper <casper.zx@gmail.com>
2013-08-18 23:27:27 +0400

    Added function for reloading app config.
    Also added function which reads all app keys.

--- src/utils_app.erl
+++ src/utils_app.erl
@@ -6,13 +6,25 @@
 %%%-------------------------------------------------------------------
 
 -module(utils_app).
--export([get_env/1, get_key/1]).
+
+-export([get_env/1]).
+-export([get_key/1, get_all_key/1]).
+
+-export([reload_configs/2]).
 
 get_env(Pars) -> lists:flatten([fun() -> case application:get_env(Par) of
 	{ok, Val} -> {Par, Val}; undefined -> [] end end() || Par <- Pars]).
 
 get_key(Key) -> get_key(application:get_application(), {key, Key}).
-get_key({ok, Application}, {key, Key}) ->
-	get_key(application:get_key(Application, Key), ok);
+get_key({ok, App}, {key, Key}) -> get_key(application:get_key(App, Key), ok);
 get_key({ok, Value}, ok) -> Value;
 get_key(undefined, _) -> [].
+
+get_all_key(App) -> case application:get_all_key(App) of
+	{ok, Keys} -> Keys; undefined -> [] end.
+
+reload_configs(Apps, Configs) ->
+	Env = application_controller:prep_config_change(),
+	application_controller:change_application_data(
+		[{application, App, get_all_key(App)} || App <- Apps], Configs),
+	application_controller:config_change(Env).
