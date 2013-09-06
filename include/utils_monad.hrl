299dd324436c1f7c549241eb06681d2e1c83ede5
casper <casper.zx@gmail.com>
2013-09-07 02:30:58 +0400

    Added macroses for better utils_monad experience.

--- include/utils_monad.hrl
+++ include/utils_monad.hrl
@@ -9,3 +9,15 @@
 -record(error, {id, reason, data}).
 -record(function, {id, name, args}).
 -record(placeholder, {id}).
+
+-define(Result(Data), [#result{data = Data}|_]).
+-define(Result(ID, Data), #result{id = ID, data = Data}).
+
+-define(Error(Reason), [#error{reason = Reason}|_]).
+-define(Error(ID, Reason, Data),
+	#error{id = ID, reason = Reason, data = Data}).
+
+-define(Function(ID, Name, Args),
+	#function{id = ID, name = Name, args = Args}).
+
+-define(Placeholder(ID), #placeholder{id = ID}).
