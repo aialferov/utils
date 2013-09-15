6d23fce861409ace765383ade37a0772e474d126
casper <casper.zx@gmail.com>
2013-09-15 11:44:06 +0400

    Added to_lower (cyrillic only) function.

--- src/utils_lists.erl
+++ src/utils_lists.erl
@@ -6,7 +6,9 @@
 %%%-------------------------------------------------------------------
 
 -module(utils_lists).
+
 -export([keyfind/2, keyfind2/2]).
+-export([to_lower/1]).
 
 keyfind(Key, List) -> keyfind(v1, Key, List).
 keyfind2(Key, List) -> keyfind(v2, Key, List).
@@ -15,3 +17,14 @@ keyfind(V, Key, List) -> case lists:keyfind(Key, 1, List) of
 	{Key, Value} -> case V of v1 -> {ok, Value}; v2 -> Value end;
 	false -> case V of v1 -> {error, not_found}; v2 -> false end
 end.
+
+to_lower(S) -> to_lower(S, []).
+to_lower([208,X|T], Acc) when 160 == X -> to_lower(T, [X + 31,208|Acc]);
+to_lower([208,X|T], Acc) when 129 == X -> to_lower(T, [X + 16,209|Acc]);
+to_lower([208,X|T], Acc) when 159 == X -> to_lower(T, [X - 31,209|Acc]);
+to_lower([208,X|T], Acc) when 144 =< X, X =< 158 ->
+	to_lower(T, [X + 32,208|Acc]);
+to_lower([208,X|T], Acc) when 159 =< X, X =< 175 ->
+	to_lower(T, [X - 32,209|Acc]);
+to_lower([H|T], Acc) -> to_lower(T, [H|Acc]);
+to_lower([], Acc) -> lists:reverse(Acc).
